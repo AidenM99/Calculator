32a56c08865654d540979c0ef74466e3e1a62dc7
Aiden McNaughton <aiden-99@hotmail.co.uk>
2021-09-01 14:14:34 +0100

    Add function for calculator display to show user input

---
+++ app.js
@@ -0,0 +1,9 @@
+const calcInput = document.querySelector(".input");
+const calcButton = document.querySelectorAll(".calculator-button");
+const clearButton = document.querySelector(".clear");
+
+calcButton.forEach (function(button) {                    //Shows user input on calculator display
+    button.addEventListener("click", function() {       
+        calcInput.append(this.innerHTML);
+    });
+});
