490b2faec5a2baa62fb7724e0fb5c41463332948
Aiden McNaughton <aiden-99@hotmail.co.uk>
2021-09-02 13:07:55 +0100

    Add function to calculate the sum of two values with a given operator

--- app.js
+++ app.js
@@ -1,13 +1,76 @@
-const calcInput = document.querySelector(".input");
-const calcButton = document.querySelectorAll(".calculator-button");
+const calcButton = document.querySelectorAll(".number");
 const clearButton = document.querySelector(".clear");
+const operators = document.querySelectorAll(".operator");
+const calcInput = document.querySelector(".input");
+const equals = document.querySelector(".equals");
 
-calcButton.forEach (function(button) {                    //Shows user input on calculator display
-    button.addEventListener("click", function() {       
-        calcInput.append(this.innerHTML);
+let operator;
+let valueOne;
+let valueTwo;
+
+calcButton.forEach(function (button) {
+    button.addEventListener("click", function () {
+// Show user input on calculator display
+        const userInput = calcInput.append(this.innerHTML);        
+// Retrieve user inputted values 
+        if (!operator) {                                           
+            valueOne = +calcInput.innerHTML;                                
+            return valueOne;
+        } else {                                                         
+            valueTwo = +calcInput.innerHTML;
+            return valueTwo;
+        };
     });
 });
 
-clearButton.addEventListener("click", function() {   //Clear calculator display
+// Clear calculator display and reset values
+const clear = clearButton.addEventListener("click", function () {   
     calcInput.innerHTML = "";
+    valueOne = "";
+    valueTwo = "";
+    operator = "";
 });
+
+// Retrieve operator
+operators.forEach(function (button) {                                              
+    button.addEventListener("click", function () {
+        calcInput.innerHTML = "";
+        operator = this.id;
+        return operator;
+    });
+});
+
+equals.addEventListener("click", function () {
+    calculate(valueOne, valueTwo, operator);
+});
+
+// Calculation function
+function calculate(valueOne, valueTwo, operator) {                       
+    switch (operator) {
+        case "add":
+            calcInput.innerHTML = valueOne + valueTwo;
+            break;
+        case "subtract":
+            calcInput.innerHTML = valueOne - valueTwo;
+            break;
+        case "multiply":
+            calcInput.innerHTML = valueOne * valueTwo;
+            break;
+        case "divide":
+            calcInput.innerHTML = valueOne / valueTwo;
+            break;
+        default:
+            console.log("Invalid Operator!");
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
